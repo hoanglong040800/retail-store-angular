0c8594176ddea1975462bd4bd2ce69a220619cba
hoanglong040800 <hoanglong040800@gmail.com>
2023-11-21 08:28:37 +0700

    feat: show list

--- src/app/pages/todo/todo.page.ts
+++ src/app/pages/todo/todo.page.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface ITodo {
+  id: number;
+  title: string;
+  desc: string;
+}
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.page.html',
-  styleUrls: [],
+  styleUrls: ['./todo.scss'],
 })
-export class TodoPage  implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+export class TodoPage {
+  todoList: ITodo[] = [
+    {
+      id: 1,
+      title: 'abc',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quidem nihil enim non eligendi neque explicabo? Excepturi ab qui illo.',
+    },
+    {
+      id: 2,
+      title: 'qwer',
+      desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, beatae. Delectus iusto voluptate nemo officia ex consequuntur! Quos, quo enim?',
+    },
+  ];
 }
