a8a572ed3f48f6bca6e2f28cc27f08edf147a09a
hoanglong040800 <hoanglong040800@gmail.com>
2023-11-28 09:28:29 +0700

    feat: show action modal

--- src/app/modules/todo/components/todo-action-modal/todo-action-modal.component.ts
+++ src/app/modules/todo/components/todo-action-modal/todo-action-modal.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'todo-action-modal',
   templateUrl: './todo-action-modal.component.html',
 })
-export class TodoActionModalComponent {}
+export class TodoActionModalComponent {
+  @ViewChild(IonModal) modal!: IonModal
+
+  onCancel() {
+    this.modal.dismiss();
+  }
+
+  onConfirm() {
+    this.modal.dismiss();
+  }
+}
--- src/app/modules/todo/components/todo.component.module.ts
+++ src/app/modules/todo/components/todo.component.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { TodoActionModalComponent } from './todo-action-modal/todo-action-modal.component';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
+  imports: [IonicModule],
   declarations: [TodoActionModalComponent],
   exports: [TodoActionModalComponent],
 })
